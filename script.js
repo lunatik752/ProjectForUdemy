bcb6423498381a444df1c8a31c393dafac3b6536
Maksim Bely <whitemaxim752@gmail.com>
2019-10-01 16:40:39 +0300

    feat: add Task 1

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-'use strict';
+let money = prompt("Ваш бюджет на месяц?", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD", ''),
+    
+    appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let answer1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    answer2 = prompt("Во сколько обойдется?", ''),
+    answer3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    answer4 = prompt("Во сколько обойдется?", '');
+
+ appData.expenses.answer1 = answer2;
+ appData.expenses.answer3 = answer4;
+
+ alert(appData.budget / 30);
+
+
+
